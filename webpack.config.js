8151138fe7b4d790b3534157dd6ba9f13410b9e1
warnine11 <147139668+warnine11@users.noreply.github.com>
2024-04-04 16:47:56 +0700

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,22 @@
 const path = require('path');
 
 module.exports = {
-    entry: "./script.js",
-    output: {
-      filename: "bundle.js",
-      path: path.resolve(__dirname, "dist"),
-    },
-    devServer: {
-      static: {
-        directory: path.join(__dirname, "dist"),
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
-      port: 8080,
-      open: true,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [
-            {
-              loader: "style-loader",
-            },
-            {
-              loader: "css-loader",
-            },
-          ],
-        },
-  
-        /* babel loader */
-        {
-          test: /\.js$/,
-          exclude: "/node_modules/",
-          use: [
-            {
-              loader: "babel-loader",
-              options: {
-                presets: ["@babel/preset-env"],
-              },
-            },
-          ],
-        },
-      ],
-    },
-    plugins: [
-      /* HTML Webpack Plugin */
-      new HtmlWebpackPlugin({
-        template: "./src/template.html",
-        filename: "index.html",
-      }),
     ],
-  };
+  },
+};
---
+++ webpack.prod.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+  },
+};
